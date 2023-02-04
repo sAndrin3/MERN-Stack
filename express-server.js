68b8c255f8cd1372069890a238c3cf592f076f04
Gearboxpetrolhead <sandrinemalanga2001@gmail.com>
2023-02-04 20:49:51 +0300

    update

--- express-server.js
+++ express-server.js
@@ -7,7 +7,7 @@ app.get("/", (req, res) => {
     res.send("<h2>Welcome to the Home Page...</h2>")
 });
 app.get("/about", (req, res) => {
-    res.send("<h2>Welcome to the About Page...</h2>")
+    res.send("<h2>Welcome to the About status Page...</h2>")
 });
 app.listen("5000", () => {
     console.log("Server running on port 5000");
